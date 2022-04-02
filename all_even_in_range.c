cb3abb57223a070008d5e626a11d4904558a2c17
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2022-04-02 21:09:24 +0530

    Create all_even_in_range.c

---
+++ all_even_in_range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i=0;
+    printf("ENTER THE VALUE OF n= ");
+    scanf("%d",&n);
+    
+    while(i<=n)
+    {
+            printf("%d\t",i);
+            i+=2;
+            
+    }
+    return 0;
+}
+
